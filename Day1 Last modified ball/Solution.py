216b03fe92ba313430bad673d785f27dbe0e9c9d
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-09 23:39:37 +0530

    Completed Solution

--- Day1 Last modified ball/Solution.py
+++ Day1 Last modified ball/Solution.py
@@ -1,11 +1,11 @@
 class Solution():
     def solve(self,N,A):
-        #code
+        for i in range(N-1,0,-1):
+            if(A[i]>=9):
+                pass
+            else:
+                return i+1
 
-
-
-
-#{
 #driver code starts
 #initial 
 for _ in range(int(input())):
@@ -13,4 +13,9 @@ for _ in range(int(input())):
     array=[int(i)for i in input().split()]
     obj=Solution()
     print(obj.solve(n,array))
-# } Driver Code ends
+# Driver Code ends
+#input use
+# n=4
+# redo n value as above 4
+# input array 3 1 4 5
+#answer 4
