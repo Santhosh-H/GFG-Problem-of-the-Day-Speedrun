92d8be4284f6ec79c7d7cb8bd894c3fe16bfd288
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-09 23:28:09 +0530

    Updated Driver Code

---
+++ Day1 Last modified ball/Solution.py
@@ -0,0 +1,16 @@
+class Solution():
+    def solve(self,N,A):
+        #code
+
+
+
+
+#{
+#driver code starts
+#initial 
+for _ in range(int(input())):
+    n=int(input())
+    array=[int(i)for i in input().split()]
+    obj=Solution()
+    print(obj.solve(n,array))
+# } Driver Code ends
