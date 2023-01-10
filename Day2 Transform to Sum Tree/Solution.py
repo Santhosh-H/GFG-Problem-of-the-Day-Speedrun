6e330d40e25f82d38332f9754fde448a326a3bde
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-10 21:08:33 +0530

    Getting and removing the front of the queue

--- Day2 Transform to Sum Tree/Solution.py
+++ Day2 Transform to Sum Tree/Solution.py
@@ -27,5 +27,31 @@ def buildTree(s):
     size=size+1
     # Starting from the second element
     i=1
-   while(size>0 and i<len(ip)):
+    while(size>0 and i<len(ip)):
         # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        # Get the current node's value from the string
+        currVal=ip[i]
+        # If the left child is not null
+        if(currVal!="N"):
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        # If the right child is not null
+        if(currVal!="N"):
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
