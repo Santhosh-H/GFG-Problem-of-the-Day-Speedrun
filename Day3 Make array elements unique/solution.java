6ee282c51c96280cb651dcafa7d06329b08ad39c
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-11 19:30:29 +0530

    Updating Driver code

--- Day3 Make array elements unique/solution.java
+++ Day3 Make array elements unique/solution.java
@@ -1,3 +1,14 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+class make{
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine().trim());
+        while(t-->0){
+            int t=0;
+            int N=Integer.parseInt(br.readLine().trim());
+            int arr[]=new int[(int)(N)];
+        }
+    }
+}
