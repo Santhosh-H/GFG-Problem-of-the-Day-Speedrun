3e720152296ac4776c74e0ecd3d25418308ee208
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-11 19:33:42 +0530

    updated in driver code

--- Day3 Make array elements unique/solution.java
+++ Day3 Make array elements unique/solution.java
@@ -9,6 +9,12 @@ class make{
             int t=0;
             int N=Integer.parseInt(br.readLine().trim());
             int arr[]=new int[(int)(N)];
+            String inputLine2[]=br.readLine().trim().split(" ");
+            for(int i=0;i<N;i++){
+                arr[i]=Integer.parseInt(inputLine2[i]);
+            }
+            Solution ob= new Solution();
+            System.out.println(ob.minIncrement(arr,N));
         }
     }
 }
