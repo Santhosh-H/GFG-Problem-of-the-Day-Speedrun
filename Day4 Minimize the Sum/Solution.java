037fa3a7ffdeb790ad4e94d43d1c78e3124fd645
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-12 22:31:04 +0530

    Updated Solution

--- Day4 Minimize the Sum/Solution.java
+++ Day4 Minimize the Sum/Solution.java
@@ -3,7 +3,15 @@ Class Solution{
     long minimizeSum(int N,int arr[]){
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>
         for(int i = 0; i < N;i++)
-        
+        pq.add(arr[i]);
+        int ans=0;
+        while(pq.size()>1){
+            int st=pq.remove();
+            int nd=pq.remove();
+            int nsum=st+nd;
+            ans=ans+nsum;
+            pq.add(nsum);
+        }
     }
 }
 
