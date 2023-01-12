62711392ddf8c9cfdb17cbe2cce2391d1f23e95d
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-12 22:26:32 +0530

    Update Solution.java

--- Day4 Minimize the Sum/Solution.java
+++ Day4 Minimize the Sum/Solution.java
@@ -4,6 +4,15 @@ public class GFG{
         int t=sc.nextInt();
         while(t-->0){
             int n=sc.nextInt();
+            int A[]=new int[n];
+            for(int i=0;i<n;i++)
+            A[i]=sc.nextInt();
+            Solution obj=new Solution();
+            long ans=obj.minimizeSum(n,A);
+            System.out.println(ans);
+        
         }
+
+
     }
 }
