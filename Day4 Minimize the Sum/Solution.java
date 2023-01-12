140440792f0d5ce3799d5b66949dc78825092090
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-12 22:23:06 +0530

    Driver Code

---
+++ Day4 Minimize the Sum/Solution.java
@@ -0,0 +1,9 @@
+public class GFG{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+        }
+    }
+}
