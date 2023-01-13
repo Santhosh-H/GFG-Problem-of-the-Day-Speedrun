917c3ad95eb5a65c0fe0c315b263a4a3af440fa6
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-13 20:23:58 +0530

    Update Solution.java

---
+++ Day5 Count Lucky Permutations/Solution.java
@@ -0,0 +1,10 @@
+//Credits Santhosh Hariharan
+import java.util.*;
+Class GFG{
+    public static void main(String[] args)throws IOException{
+            BufferedReader read=new BufferedReader(new InputStreamReader(System.in));
+            int t= Interger.parsInt(read.readLine());
+            while(t-->0){
+                
+    }
+}
