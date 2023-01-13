194c3f89f60f1ea5604c7d2be0059f6fd28a344e
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-13 20:27:18 +0530

    Driver Code

--- Day5 Count Lucky Permutations/Solution.java
+++ Day5 Count Lucky Permutations/Solution.java
@@ -1,10 +1,16 @@
 //Credits Santhosh Hariharan
+//Driver code
 import java.util.*;
 Class GFG{
     public static void main(String[] args)throws IOException{
             BufferedReader read=new BufferedReader(new InputStreamReader(System.in));
             int t= Interger.parsInt(read.readLine());
             while(t-->0){
-                
+            String s[]=read.readLine();.split(" ");
+            int N =Integer.parseInt(s[0]);
+            int M=Integer.parseInt(s[1]);
+            int arr[]=new int[(int)(N)];
+            int i=0;
+            String inputLine2[]=read.readLine().trim().split(" ");
     }
 }
