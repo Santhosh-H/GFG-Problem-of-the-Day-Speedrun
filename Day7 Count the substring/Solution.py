907128460ad7a6139a55029170a73e9590408d03
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-15 14:29:33 +0530

    driver Code

---
+++ Day7 Count the substring/Solution.py
@@ -0,0 +1,9 @@
+#this code was done By santhosh hariharan
+#Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    for _ in range(int(input(())):
+        S = input()
+        print(Solution().countSubstring(s))
+#  Driver Code Ends
+
