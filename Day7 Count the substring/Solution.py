a918f9ce9eed152ee85174e3294e636ecc266123
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-15 14:31:21 +0530

    Completed Solution

--- Day7 Count the substring/Solution.py
+++ Day7 Count the substring/Solution.py
@@ -16,3 +16,16 @@ class Solution():
         x1[0]=1
         x2[1]=1
         ans=0
+        for i in S:
+             
+            if i=='1':
+                ones = 1
+            else:
+                ones =-1
+            count=count+ones
+            x1[count] += 1+x2[count]
+            x2[count+1] += 1+x2[count]
+            x2[count]=0
+            ans=ans+x1[count-1]
+        return ans
+#this code was done By santhosh hariharan
