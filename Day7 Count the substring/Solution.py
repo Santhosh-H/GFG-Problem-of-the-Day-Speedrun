71f8e6ba6a36ae0fa38d3dbfef13899abc43694e
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-15 14:30:14 +0530

    Updated Solution

--- Day7 Count the substring/Solution.py
+++ Day7 Count the substring/Solution.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 #this code was done By santhosh hariharan
 #Driver Code Starts
 #Initial Template for Python 3
@@ -6,4 +7,12 @@ if __name__ == '__main__':
         S = input()
         print(Solution().countSubstring(s))
 #  Driver Code Ends
-
+class Solution():
+    def countSubstring(self, S):
+        #your code here
+        x1=defaultdict(int)
+        x2=defaultdict(int)
+        count=0
+        x1[0]=1
+        x2[1]=1
+        ans=0
